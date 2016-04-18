acd16cafab6d5a054048a6be4de3ec79eb31112c
JennyLi90 <admin@Tobys-MacBook-Pro.local>
2016-04-18 19:43:33 +0800

    try ios-level 1
    set background for yellow color.code school-level 1

---
+++ learning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learning
+//
+//  Created by admin on 4/18/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ learning/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  learning
+//
+//  Created by admin on 4/18/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor yellowColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
